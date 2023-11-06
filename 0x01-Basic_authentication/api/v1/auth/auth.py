071c75ed4ac99c9bc90e35e8ef6f93d5a44f43c9
NewtonMutugi <newtonmut197@gmail.com>
2023-11-07 00:41:44 +0300

    Increased length of auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-"""Authorization module"""
+"""Authorization module for API"""
 from typing import List, TypeVar
-from flask import request
 
 
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Require auth method"""
+        """Require auth method
+        return: boolean value
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """Authorization header method"""
+        """Authorization header method
+        return: string or None"""
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Current User method"""
+        """Current User method
+        return: User or None"""
         return None
