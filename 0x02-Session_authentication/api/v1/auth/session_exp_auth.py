f1ebf60075791ccb322318f8f7492af74701d4ab
NewtonMutugi <newtonmut197@gmail.com>
2023-11-27 00:30:58 +0300

    session_exp_auth.py: modified

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -10,11 +10,12 @@ class SessionExpAuth(SessionAuth):
 
     def __init__(self) -> None:
         """Constructor"""
+        super().__init__()
         SESSION_DURATION = getenv('SESSION_DURATION')
 
         try:
             session_duration = int(SESSION_DURATION)
-        except Exception:
+        except (ValueError, TypeError):
             session_duration = 0
 
         self.session_duration = session_duration
@@ -27,7 +28,7 @@ class SessionExpAuth(SessionAuth):
 
         session_dictionary = {
             'user_id': user_id,
-            'created_at': self.session_duration
+            'created_at': datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         }
         self.user_id_by_session_id[session_id] = session_dictionary
         return session_id
@@ -35,20 +36,20 @@ class SessionExpAuth(SessionAuth):
     def user_id_for_session_id(self, session_id=None):
         """Returns a User ID based on a Session ID"""
 
-        if session_id is None:
+        if session_id is None or session_id not in self.user_id_by_session_id:
             return None
-        if session_id not in self.user_id_by_session_id.keys():
-            return None
+
         session_dictionary = self.user_id_by_session_id.get(session_id)
 
-        if session_dictionary is None:
-            return None
-        if self.session_duration <= 0:
+        if session_dictionary is None or self.session_duration <= 0:
             return session_dictionary.get('user_id')
+
         created_at = session_dictionary.get('created_at')
 
         if created_at is None:
             return None
+
+        created_at = datetime.strptime(created_at, '%Y-%m-%d %H:%M:%S')
         expired_time = created_at + timedelta(seconds=self.session_duration)
 
         if expired_time < datetime.now():
