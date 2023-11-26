ebaa408e0ea17bbedf73fdfe5d9ed29bcfa94540
NewtonMutugi <newtonmut197@gmail.com>
2023-11-27 00:20:29 +0300

    session_exp_auth.py: configured the auth

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Session Expiration Authentication"""
+from datetime import datetime, timedelta
 from os import getenv
 from .session_auth import SessionAuth
 
@@ -9,8 +10,14 @@ class SessionExpAuth(SessionAuth):
 
     def __init__(self) -> None:
         super().__init__()
-        session_duration = getenv('SESSION_DURATION')
-        self.session_duration = int(session_duration)
+        SESSION_DURATION = getenv('SESSION_DURATION')
+
+        try:
+            session_duration = int(SESSION_DURATION)
+        except Exception:
+            session_duration = 0
+
+        self.session_duration = session_duration
 
     def create_session(self, user_id=None):
         """Creates a Session ID for a user_id"""
@@ -27,23 +34,24 @@ class SessionExpAuth(SessionAuth):
 
     def user_id_for_session_id(self, session_id=None):
         """Returns a User ID based on a Session ID"""
+
         if session_id is None:
             return None
-
         if session_id not in self.user_id_by_session_id.keys():
             return None
-
         session_dictionary = self.user_id_by_session_id.get(session_id)
+
         if session_dictionary is None:
             return None
+        if self.session_duration <= 0:
+            return session_dictionary.get('user_id')
+        created_at = session_dictionary.get('created_at')
 
-        if 'created_at' not in session_dictionary.keys():
+        if created_at is None:
             return None
+        expired_time = created_at + timedelta(seconds=self.session_duration)
 
-        if 'user_id' not in session_dictionary.keys():
-            return None
-
-        if session_dictionary.get('created_at') < 0:
+        if expired_time < datetime.now():
             return None
 
         return session_dictionary.get('user_id')
